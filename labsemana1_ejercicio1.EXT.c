109917c2e3f1827d6c3ba2d3cbc0efbb6a6efa2a
Rodrigo Soto Véjar <rodrigo.osvaldo.2003@gmail.com>
2023-03-18 22:30:34 -0300

    ejercicio 1

---
+++ labsemana1_ejercicio1.EXT.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	
+	int Entero=1;
+	char Caracter='a';
+	float Realf;
+	Realf=(float)1/3;
+	printf("dato entero %d\n",Entero);
+	printf("dato caracter %c\n",Caracter);
+	printf("dato float %f",Realf);
+	
+	return 0;
+}
