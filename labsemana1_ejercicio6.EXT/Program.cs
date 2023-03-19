b7409e99247f5511d0893bf44e87cf123904950e
Rodrigo Soto Véjar <rodrigo.osvaldo.2003@gmail.com>
2023-03-18 22:42:57 -0300

    ejercicio 6 lab 1 c#

---
+++ labsemana1_ejercicio6.EXT/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace labsemana1_ejercicio6.EXT
+{
+    class Program{
+        static void Main(string[] args)
+        {
+            string _clase="Hechizero";
+            string _nombre="Rodrigo";
+            int _vida=450;
+            int _poder=45;
+            int _velocidad=78;
+            jugador pl1=new jugador(_clase,_nombre,_vida,_poder,_velocidad);
+            Console.WriteLine(pl1.ToString());
+            pl1.Descansar();
+            Console.WriteLine(pl1.ToString());
+            pl1.Atacar(_poder);
+
+        }
+
+    }
+}
